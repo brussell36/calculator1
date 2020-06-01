9d7f5097253a913be437dc9aa089634e17e6b3e5
Joseph Pearce <joseph.pearce05@gmail.com>
2020-06-01 10:45:28 -0700

    add co-contributors
    Co-authored-by: Ben Russell benjaminrussell36@gmail.com
    Co-authored-by: Thomas Glenn thomasgrantglenn@mac.com

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,64 @@
 
 // prompt("what is your name");
 
-let name = function(sayHello) {
-  return sayHello;
-}
+// let name = function(sayHello) {
+//   return sayHello;
+// }
 
-console.log(name("Thomas"));
-
-
-const subtract = function(num1, num2) {
-  return num1 - num2;
-}
-
-console.log(subtract(7, 5));
+// console.log(name("Thomas"));
 
 
+// const subtract = function(num1, num2) {
+//   return num1 - num2;
+// }
 
+// console.log(subtract(7, 5));
+
+// const multiply = function(int1, int2, int3) {
+//   return int1 * int2 * int3;
+// }
+
+// console.log(multiply(3, 7, 2 ));
+
+// function divide(numb1, numb2) {
+//   return numb1 / numb2;
+// }
+
+// console.log(divide(21,7));
+
+// function remainder(numb1, numb2) {
+//   return numb1 % numb2;
+// }
+
+// console.log(remainder(21,6));
+
+// let userAge = prompt("What is your age?");
+// let userFood = prompt("What is your favorite food?");
+
+// function ageFood() {
+//   return userAge + " " + userFood;
+// }
+
+// alert(ageFood());
+
+const add = function(number1, number2) {
+  return number1 + number2;
+};
+
+const subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+const multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+const divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const result = add(number1, number2);
+alert(result);
