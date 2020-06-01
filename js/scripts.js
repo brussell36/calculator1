791768ce8edb1fc98354283645afe3ae96d008d6
Joseph Pearce <joseph.pearce05@gmail.com>
2020-06-01 11:47:11 -0700

    add function - convert to Celsius, to Liter

--- js/scripts.js
+++ js/scripts.js
@@ -46,24 +46,63 @@
 
 // alert(ageFood());
 
-const add = function(number1, number2) {
-  return number1 + number2;
-};
+// const add = function(number1, number2) {
+//   return number1 + number2;
+// };
 
-const subtract = function(number1, number2) {
-  return number1 - number2;
-};
+// const subtract = function(number1, number2) {
+//   return number1 - number2;
+// };
 
-const multiply = function(number1, number2) {
-  return number1 * number2;
-};
+// const multiply = function(number1, number2) {
+//   return number1 * number2;
+// };
 
-const divide = function(number1, number2) {
-  return number1 / number2;
-};
+// const divide = function(number1, number2) {
+//   return number1 / number2;
+// };
 
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
-alert(result);
+// const number1 = parseInt(prompt("Enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
+// const result = add(number1, number2);
+// alert(result);
+
+const celsiusToFahrenheit = function(numberC) {
+  return numberC * 1.8 + 32;
+  }
+
+  let numberC = 10;
+  // celsiusToFahrenheit(10);
+  // const numberC = parseInt(prompt("Enter a number"));
+
+
+
+  console.log(celsiusToFahrenheit(numberC));
+
+const fahrenheitToCelsius = function(numberF) {
+  return (numberF - 32) / 1.8 ;
+}
+
+let numberF = 70;
+
+console.log(fahrenheitToCelsius(numberF));
+
+
+
+function convertToFahren(celsius) {
+  let fahren = celsius * 9/5 + 32;
+  return fahren;
+}
+
+console.log(convertToFahren(30));
+
+
+function galToLiter(gallon) {
+  let liter = gallon * 3.785;
+  return liter;
+}
+// let gallon = 1;
+// console.log(galToLiter(10))
+let gallon = parseInt(prompt("How many gallons?"));
+console.log(galToLiter(gallon));
