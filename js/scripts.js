a6188b02eaa268fdfabf503a147ec1a96072b1c9
Joseph Pearce <joseph.pearce05@gmail.com>
2020-06-01 09:54:26 -0700

    add functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
-const add = function(num1, num2) {
-  return num1 + num2;
-};
+// const add = function(num1, num2) {
+//   return num1 + num2;
+// };
 
-console.log(add(5, 7));
+// console.log(add(5, 7));
+
+// prompt("what is your name");
+
+let name = function(sayHello) {
+  return sayHello;
+}
+
+console.log(name("Thomas"));
 
 
+const subtract = function(num1, num2) {
+  return num1 - num2;
+}
+
+console.log(subtract(7, 5));
 
 
 
